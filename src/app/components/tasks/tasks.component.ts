e0981c1a4f814c9aa73f8259d735eafe95b06704
Mati <maticristovao@hotmail.com>
2023-01-07 17:47:19 -0300

    service and task server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/Task';
-
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +8,14 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit{
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor(){}
+  constructor(private taskService:TaskService){}
 
   ngOnInit(): void {
-    
+    // Like promise
+    this.taskService.getTasks().subscribe((tasks)=>{
+      this.tasks = tasks;
+    });
   }
 }
